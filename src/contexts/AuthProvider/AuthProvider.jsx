fc947c840c4c449be0106f42ab4261d327cd4dd2
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-07-07 23:44:56 +0600

    ThriveSecure AuthProvider-setup done.

--- src/contexts/AuthProvider/AuthProvider.jsx
+++ src/contexts/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from "../AuthContext/AuthContext";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import { auth } from '../../service/firebase.config';
 
-const AuthProvider = () => {
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const googleProvider = new GoogleAuthProvider();
+    const googleLogin = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const updateUser = (updatedData) => {
+        return updateProfile(auth.currentUser, updatedData);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => {
+            unSubscribe();
+        }
+    }, []);
+
+    const logoutUser = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    const authInfo = {
+        user,
+        setUser,
+        loading,
+        setLoading,
+        createUser,
+        updateUser,
+        googleLogin,
+        loginUser,
+        logoutUser
+    };
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
     );
 };
 
