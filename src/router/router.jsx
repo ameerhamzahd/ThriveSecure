5b39bd986e625fa0f96a7db579bd42d4b693f8a7
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-07-09 19:23:33 +0600

    ThriveSecure AdminRoutes-setup done.

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
 import RootLayout from '../layouts/RootLayout/RootLayout';
+import DashboardLayout from '../layouts/DashboardLayout/DashboardLayout';
 import Home from '../pages/Home/Home';
 import NotFound from '../pages/NotFound/NotFound';
 import Login from '../pages/Login/Login';
 import Register from '../pages/Register/Register';
 import Quote from '../pages/Quote/Quote';
 import Application from '../pages/Application/Application';
+import Dashboard from '../pages/Dashboard/Dashboard';
 
 export const router = createBrowserRouter([
   {
@@ -36,4 +38,14 @@ export const router = createBrowserRouter([
       },
     ]
   },
+  {
+    path: "/dashboard",
+    Component: DashboardLayout,
+    children: [
+      {
+        index: true,
+        Component: Dashboard
+      }
+    ]
+  }
 ]);
