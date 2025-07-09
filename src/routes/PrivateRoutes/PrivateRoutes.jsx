afa3179c236eabdcb9a2cc3643cb49a0e39bcb47
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-07-09 19:31:40 +0600

    ThriveSecure PrivateRoutes-setup done.

--- src/routes/PrivateRoutes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes/PrivateRoutes.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Navigate, useLocation } from 'react-router';
+import Loader from '../../components/shared/Loader/Loader';
+import useAuth from '../../hooks/useAuth/useAuth';
 
-const PrivateRoutes = () => {
+const PrivateRoutes = ({ children }) => {
+    const { user, loading } = useAuth();
+    const location = useLocation();
 
+    if (loading) {
+        return <Loader></Loader>;
+    }
 
+    if (user && user?.email) {
+        return children;
+    }
 
-    return (
-        <div>
-            
-        </div>
-    );
+    return <Navigate to="/login" state={location.pathname}></Navigate>;
 };
 
 export default PrivateRoutes;
