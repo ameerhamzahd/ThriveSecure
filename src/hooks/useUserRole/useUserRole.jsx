d5f567d2f26f50c03d13eb215da82f2c6ae91f5f
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-07-09 16:42:19 +0600

    ThriveSecure useUserRole-Hook-Setup done.

---
+++ src/hooks/useUserRole/useUserRole.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import useAuth from '../useAuth/useAuth';
+import useAxiosSecure from '../useAxiosSecure/useAxiosSecure';
+
+const useUserRole = () => {
+    const { user, loading } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const {
+        data: role = "customer",
+        isPending,
+        isLoading,
+        isError,
+      } = useQuery({
+        queryKey: ["user-role", user?.email],
+        enabled: !!user?.email && !loading, // only fetch when email is available
+        queryFn: async () => {
+          const res = await axiosSecure.get(`/users/${user.email}/role`);
+          return res.data.role;
+        },
+      });
+    
+      return { role, isLoading: isPending || isLoading, isError };
+};
+
+export default useUserRole;
